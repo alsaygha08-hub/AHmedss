d42a67ef9a6e75d9887b7c4e6793ee1f64e7cc3e
alsaygha08-hub <alsaygha08@gmail.com>
2025-08-13 21:12:05 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ const ASSETS = [
 ];
 self.addEventListener('install', e=>{
  // e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+ // self.skipWaiting();
 });
 self.addEventListener('activate', e=>{
  // e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
