bab2bbd70a6fe013c87bf28aa8afa85e55f6c69b
alsaygha08-hub <alsaygha08@gmail.com>
2025-08-13 21:38:24 +0300

    Add files via upload

---
+++ prompts_local.js
@@ -0,0 +1,10 @@
+window.PROMPTS_LOCAL = [
+  { text: "Your co-worker Omar wants to know about the time required to set up a new drilling operation. Tell Omar the range. Use the table to help you.", audio: "g1.mp3", image: "1.png" },
+  { text: "Your co-worker Eman wants to know about the production rate of oil wells. Tell Eman the range. Use the table to help you.", audio: "g2.mp3", image: "2.png" },
+  { text: "Your new co-worker Yasser wants to know how long communication meetings last. Tell Yasser the range. Use the table to help you.", audio: "g3.mp3", image: "3.png" },
+  { text: "Your co-worker Nada asks you about the project costs. Tell Nada the range. Use the table to help you.", audio: "g4.mp3", image: "4.png" },
+  { text: "Your co-worker Mohamed asks you how long it takes to write reports. Tell Mohamed the range. Use the table to help you.", audio: "g5.mp3", image: "5.png" },
+  { text: "Your co-worker Mona asks you about the number of employees in each department. Tell Mona the range. Use the table to help you.", audio: "g6.mp3", image: "6.png" },
+  { text: "Your co-worker Ibrahim asks you about the amount of water required to put out an industrial fire. Tell Ibrahim the range. Use the table to help you.", audio: "g7.mp3", image: "7.png" },
+  { text: "Your co-worker Nouf asks you about the humidity range. Tell her. Use the table to help you.", audio: "g8.mp3", image: "8.png" },
+];
